62694acbf821ddae86cb6d19c920bd313ece2d3f
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-25 19:44:21 +0100

    let's finish this: real data is returned.

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
-import { app } from 'mu';
-app.get('/book-reports', function( req, res ) {
-  res.send( { message: `Welcome to book-reports` } );
+import { app, query, sparqlEscapeUri } from 'mu';
+
+app.get('/book-reports', async function( req, res ) {
+  let reportsData = await getReports();
+  let jsonData = reportsDataToJsonApiObject(reportsData);
+  res.send( jsonData );
 });
+
+async function getReports(){
+  const results = await query(`
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+
+    SELECT ?uri ?uuid ?created ?booksTotal WHERE {
+      GRAPH <http://mu.semte.ch/application> {
+         ?uri a ext:BookReport;
+            mu:uuid ?uuid;
+            dct:created ?created;
+            ext:booksTotal ?booksTotal.
+      }
+
+    }
+    ORDER BY DESC(?created)
+  `);
+
+  return results.results.bindings;
+}
+
+function reportsDataToJsonApiObject(reportsData){
+
+  const entries = reportsData.map( reportData => {
+    return {
+      attributes: {
+        created: reportData.created.value,
+        uri: reportData.uri.value,
+        'books-total': parseInt(reportData.booksTotal.value)
+      },
+      id: reportData.uuid.value,
+      type: "book-report",
+      relationships: {}
+    };
+  });
+
+  return {
+    data: entries,
+    links: {
+      "first": "/book-reports",
+      "last": "/book-reports"
+    }
+  };
+}
